d436a1ef17086f985044098fbcef54a11b8eaba0
dah9 <fdsgs@gmail.com>
2025-02-18 01:42:38 +0500

    [FIX] Fix problems with tests

--- bot/src/main/java/com/github/config/BotConfig.java
+++ bot/src/main/java/com/github/config/BotConfig.java
@@ -4,11 +4,13 @@ package com.github.config;
 import com.github.bot.LinkTrackerBot;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
 @Configuration
+@Profile("!test")
 public class BotConfig {
 
     @Bean
--- bot/src/test/java/com/github/BotApplicationTests.java
+++ bot/src/test/java/com/github/BotApplicationTests.java
@@ -4,10 +4,12 @@ import com.github.bot.LinkTrackerBot;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
+import org.springframework.test.context.ActiveProfiles;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class BotApplicationIntegrationTests {
 
 	@Test
